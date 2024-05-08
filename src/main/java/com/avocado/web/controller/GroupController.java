eeadae76ab4b2a77f56d3394b12bfe2e33064867
nallume <hoxy910@gmail.com>
2024-05-08 17:48:32 +0900

    집단상담페이지메인

--- src/main/java/com/avocado/web/controller/GroupController.java
+++ src/main/java/com/avocado/web/controller/GroupController.java
@@ -1,7 +1,13 @@
 package com.avocado.web.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.avocado.web.entity.GroupDTO;
 import com.avocado.web.service.GroupService;
 
 import jakarta.annotation.Resource;
@@ -11,5 +17,35 @@ public class GroupController {
 	
 	@Resource(name="groupService")
 	private GroupService groupService;
-
+	
+	//프로그램 확인 페이지 (학생)
+	@GetMapping("/groupMain")
+	public String groupMain(Model model) {
+		List<GroupDTO> list = groupService.programList();
+		model.addAttribute("list", list);
+		return "program/groupMain";
+	}
+	
+	//프로그램 신청
+	@GetMapping("/programApply")
+	public String programApply() {
+		
+		return "program/programApply";
+	}
+	
+	
+	//프로그램 확인 페이지 (상담사)
+	@GetMapping("/admin/groupPrograms")
+	private String groupPrograms() {
+		
+		return "admin/groupPrograms";
+	}
+	
+	//프로그램 등록 페이지(상담사)
+	@PostMapping("/admin/registProgram")
+	private String registProgram() {
+		
+		return "admin/registProgram";
+	}
+	
 }
--- src/main/java/com/avocado/web/service/GroupServiceImpl.java
+++ src/main/java/com/avocado/web/service/GroupServiceImpl.java
@@ -1,16 +1,22 @@
 package com.avocado.web.service;
 
 import java.util.List;
-import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.avocado.web.entity.GroupDTO;
+import com.avocado.web.repository.GroupDAO;
 
 @Service("groupService")
 public class GroupServiceImpl implements GroupService {
+	
+	@Autowired
+	private GroupDAO groupDAO;
 
 	@Override
-	public List<Map<String, Object>> test() {
-		return null;
+	public List<GroupDTO> programList() {
+		return groupDAO.programList();
 	}
 	
 }
