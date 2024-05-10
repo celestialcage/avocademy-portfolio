52be51ed40a15b4f0d6892e23194345fd52eba03
Jinseon Lee <leaftree@kakao.com>
2024-05-10 17:49:11 +0900

    캘린더와의 싸움...

---
+++ src/main/resources/static/admin/custom/app.js
@@ -0,0 +1,9 @@
+const addEventBtn = document.querySelector('#addEventBtn');
+
+function addEBHandler() {
+    alert("add event");
+    console.log('clicked');
+}
+
+addEventBtn.addEventListener("click", addEBHandler);
+
