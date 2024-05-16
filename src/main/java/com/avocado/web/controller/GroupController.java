ff8ec1985dea821f49cf6982c144d3616279a942
nallume <hoxy910@gmail.com>
2024-05-16 18:57:20 +0900

    그룹

--- src/main/java/com/avocado/web/controller/GroupController.java
+++ src/main/java/com/avocado/web/controller/GroupController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.avocado.web.entity.GroupDTO;
 import com.avocado.web.service.GroupService;
@@ -13,30 +15,30 @@ import com.avocado.web.service.GroupService;
 import jakarta.annotation.Resource;
 
 @Controller
+@RequestMapping("/group")
 public class GroupController {
 	
 	@Resource(name="groupService")
 	private GroupService groupService;
 	
 	//프로그램 확인 페이지 (학생)
-	@GetMapping("/group")
-	public String groupMain(Model model) {
-		List<GroupDTO> list = groupService.programList();
-		model.addAttribute("list", list);
+	@GetMapping("")
+	public String groupMain() {
 		return "program/group";
 	}
 	
 	@GetMapping("/programList")
-	public String programList(Model model) {
-		
+	public String programList(Model model) {		
+		List<GroupDTO> list = groupService.programList();
+		model.addAttribute("list", list);
 		return "program/programList";
 	}
 	
 	//프로그램 신청(학생)
-	@GetMapping("/groupApply")
-	public String groupApply() {
+	@GetMapping("/programApply")
+	public String groupApply(@RequestParam("no") String no) {
 		
-		return "program/groupApply";
+		return "program/programApply";
 	}
 	
 	
