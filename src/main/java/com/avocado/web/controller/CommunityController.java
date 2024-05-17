b74eb0b4b9c43317fe4e64b3a58f4e62689ac4f4
sukgi094 <dmsdbdl@gmail.com>
2024-05-17 19:35:29 +0900

    커뮤니티 실패!! 중간 저장

--- src/main/java/com/avocado/web/controller/CommunityController.java
+++ src/main/java/com/avocado/web/controller/CommunityController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.avocado.web.entity.OnlineDTO;
@@ -16,7 +17,9 @@ import com.avocado.web.util.Util;
 
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpSession;
+
 @Controller
+@RequestMapping("/community")
 public class CommunityController {
 	
 	@Autowired
@@ -31,7 +34,14 @@ public class CommunityController {
 		System.out.println(bno);
 		OnlineDTO detail = onlineService.detail(bno);
 		System.out.println(detail.toString());
-		if (detail.getUname().equals(session.getAttribute("uname")) || (int) session.getAttribute("ugrade") == 5) {
+
+		// 세션에 로그인이 안되어있는 상태에서 session.get 하면 null 반환되고
+		// null 이랑 equals 연산을 하다보니 null 오류가 발생
+		// 로그인이 안되어있으면 다시 게시판 페이지로 로딩이되거나 로그인으로 이동시키거나 골라서
+		if(session.getAttribute("uname") == null) {
+			// return "redirect:/login";
+			return "redirect:/online";
+		} else if (detail.getUname().equals(session.getAttribute("uname")) || (int) session.getAttribute("ugrade") == 5) {
 			model.addAttribute("detail", detail);
 			return "online/detail";
 		} else {
