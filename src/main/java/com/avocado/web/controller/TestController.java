f8a8e7e3264659c86caa5c846144f9c41717cf46
Jinseon Lee <leaftree@kakao.com>
2024-05-18 15:21:46 +0900

    TestController 오류 수정

--- src/main/java/com/avocado/web/controller/TestController.java
+++ src/main/java/com/avocado/web/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.avocado.web.controller;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -9,14 +8,17 @@ import java.util.Map;
 
 import javax.net.ssl.HttpsURLConnection;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.avocado.web.service.SecureInfo;
 import com.avocado.web.service.TestService;
+import com.avocado.web.util.SecureInfo;
 
 import jakarta.annotation.Resource;
 
