73ce56a4dac9c0a7278fc5ac53217d855784844a
zzang9 <shcoder1226@gmail.com>
2024-05-18 14:23:10 +0900

    detail css수정

--- src/main/java/com/avocado/web/controller/OnlineController.java
+++ src/main/java/com/avocado/web/controller/OnlineController.java
@@ -1,6 +1,7 @@
 package com.avocado.web.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,8 @@ public class OnlineController {
 	@Resource(name = "onlineService")
 	private OnlineService onlineService;
 
+	// 상세보기
+
 	@GetMapping("/detail")
 	public String detail(Model model, @RequestParam(name = "bno", required = false, defaultValue = "1") int bno,
 			HttpSession session) {
@@ -38,16 +41,30 @@ public class OnlineController {
 		// 세션에 로그인이 안되어있는 상태에서 session.get 하면 null 반환되고
 		// null 이랑 equals 연산을 하다보니 null 오류가 발생
 		// 로그인이 안되어있으면 다시 게시판 페이지로 로딩이되거나 로그인으로 이동시키거나 골라서
-		if(session.getAttribute("uname") == null) {
+		if (session.getAttribute("uname") == null) {
 			// return "redirect:/login";
 			return "redirect:/online";
-		} else if (detail.getUname().equals(session.getAttribute("uname")) || (int) session.getAttribute("ugrade") == 5) {
+		} else if (detail.getUname().equals(session.getAttribute("uname"))
+				|| (int) session.getAttribute("ugrade") == 5) {
 			model.addAttribute("detail", detail);
 			return "online/detail";
 		} else {
 			return "redirect:/online";
 		}
 	}
+
+	/*
+	 * @GetMapping("/detail") public String detail(@RequestParam(name = "bno")
+	 * String bno, Model model) { int reNo = util.str2Int(bno); if (reNo != -1) {
+	 * OnlineDTO onlineDTO = onlineService.detail(reNo); if (onlineDTO != null) {
+	 * model.addAttribute("detail", onlineDTO); if (onlineDTO.getComment() > 0) {
+	 * List<OnlineDTO> commentList = onlineService.commentList(reNo);
+	 * model.addAttribute("comment", commentList); } return "detail"; } else {
+	 * return "redirect:/error"; // 데이터가 없는 경우 처리 } } else { return
+	 * "redirect:/error"; // bno가 유효하지 않은 경우 처리 } }
+	 */
+
+	// 글쓰기1
 
 	@GetMapping("/write")
 	public String write() {
@@ -61,6 +78,8 @@ public class OnlineController {
 			return "redirect:/login";
 		}
 	}
+
+	// 글쓰기2
 
 	@PostMapping("/write")
 	public String write(@RequestParam(name = "btitle") String btitle, @RequestParam(name = "bcontent") String bcontent,
@@ -93,12 +112,20 @@ public class OnlineController {
 
 	}
 
+	// 글삭제
+
 	@PostMapping("/deletecd")
-	public String deletecd(@RequestParam(name = "bno") String bno) {
+	public String deletecd(@RequestParam(name = "bno") String bno, HttpSession session) {
 		System.out.println("삭제 : " + bno);
 		int result = onlineService.deletecd(bno);
 		return "redirect:/online";
 	}
-	
+
+	// 댓글달기
+	@PostMapping("/comment")
+	public String comment(@RequestParam(name = "bno") String bno, HttpSession session) {
+		
+		return "redirect:/detail";
+	}
 
 }
