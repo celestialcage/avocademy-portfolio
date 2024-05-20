395672112dfbc168f6e876374a29c17810f4b222
sukgi094 <dmsdbdl@gmail.com>
2024-05-20 17:11:01 +0900

    파일업로드-첨부X 글쓰기 성공

--- src/main/java/com/avocado/web/service/CommunityServiceImpl.java
+++ src/main/java/com/avocado/web/service/CommunityServiceImpl.java
@@ -57,37 +57,42 @@ public class CommunityServiceImpl implements CommunityService {
 
 	@Override
 	public Map<String, Object> write(Map<String, Object> map, FilesDTO dto, MultipartFile file) {
-			System.out.println("서비스 맵"+map);
-			System.out.println("서비스_파일 타입: " + file.getName());
-			System.out.println("서비스_파일 사이즈: " + file.getSize());
-			System.out.println("서비스_파일 이름: " + file.getOriginalFilename());
+	//		System.out.println("서비스 맵"+map);
+	//		System.out.println("서비스_파일 타입: " + file.getName());
+	//		System.out.println("서비스_파일 사이즈: " + file.getSize());
+	//		System.out.println("서비스_파일 이름: " + file.getOriginalFilename());
 
 			// 서버의 물리적 경로
-			 String uploadDir = System.getProperty("user.dir") + "/src/main/resources/static/files"; // 업로드 디렉토리 경로
+			String uploadDir = System.getProperty("user.dir") + "/src/main/resources/static/files"; // 업로드 디렉토리 경로
+			
+			
+			// 파일이 비어 있지 않은 경우에만 파일 처리 로직을 수행
+		    if (!file.isEmpty()) {
+			
 			String filePath = uploadDir + "/" + file.getOriginalFilename(); // 파일 경로 설정
 			// 디렉터리가 존재하지 않으면 생성
 			File url = new File(uploadDir);
 			if (!url.exists()) {
 				url.mkdirs();
 			}
-			url.mkdirs();
+			
 			
 			// UUID 생성
 			UUID uuid = UUID.randomUUID();
-			System.out.println("원본파일명 : " + file.getOriginalFilename());
-			System.out.println("UUID파일명 : " + uuid.toString() + file.getOriginalFilename());
+	//		System.out.println("원본파일명 : " + file.getOriginalFilename());
+	//		System.out.println("UUID파일명 : " + uuid.toString() + file.getOriginalFilename());
 	
 			String newFileName = uuid.toString() + "-" + file.getOriginalFilename();
-			System.out.println("새로 만들어진 파일이름 : " + newFileName);
+	//		System.out.println("새로 만들어진 파일이름 : " + newFileName);
 	
 			// 날짜를 뽑아서 파일명 변경하기
 			LocalDateTime Idt = LocalDateTime.now();
 			String IdtFormat = Idt.format(DateTimeFormatter.ofPattern("YYYYMMddHHmmSS"));
-			System.out.println("날짜 파일명 : " + IdtFormat + file.getOriginalFilename());
+	//		System.out.println("날짜 파일명 : " + IdtFormat + file.getOriginalFilename());
 	
 			// 실제 경로
 			File upFileName = new File(url, file.getOriginalFilename());
-			System.out.println("실제 경로 : " + url);
+	//		System.out.println("실제 경로 : " + url);
 	
 			try {
 				// 서버에 파일저장
@@ -109,13 +114,21 @@ public class CommunityServiceImpl implements CommunityService {
 		  } catch (IllegalStateException | IOException e) {
 		        e.printStackTrace();
 		    }
+			
+		  }
 			Map<String, Object> result = new HashMap<String, Object>();
 		    // 글 작성 정보 저장
 			int write = communityDAO.write(map);
-			int fileUp = communityDAO.fileUp(dto);
+			
+			
+			// 파일이 비어 있지 않은 경우에만 파일 정보를 데이터베이스에 저장
+		    if (!file.isEmpty()) {
+		        int fileUp = communityDAO.fileUp(dto);
+		        result.put("fileUp", fileUp);
+		    }
+
 			
 			result.put("write", write);
-			result.put("fileUp", fileUp);
 			
 			return result;
 		}
